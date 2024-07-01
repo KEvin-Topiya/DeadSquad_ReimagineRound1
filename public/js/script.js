5a8e5fd2616609d60334cd65770bf2b8846eaf58
Kish-v17 <kish.v07@gmail.com>
2024-07-01 18:48:18 +0530

    CustomCursor

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,13 @@
+//=============== Kishan code================
+var csr = document.querySelector("#cursor")
+document.addEventListener("mousemove",function(loc){
+    csr.style.left=loc.x-20+"px";
+    csr.style.top=loc.y-20+"px";
+})
+
+
+// End Kishan
+
 // Rixit Code
 
 // End Rixit 
