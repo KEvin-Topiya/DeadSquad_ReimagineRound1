f9e3c11ac33105fec32305376c5fd6f48382425b
Kish-v17 <kish.v07@gmail.com>
2024-07-01 19:10:45 +0530

    Scrolltrigger

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,21 @@ document.addEventListener("mousemove",function(loc){
     csr.style.left=loc.x-15+"px";
     csr.style.top=loc.y-15+"px";
 })
+gsap.to(".nav",{
+    backgroundColor:"#FFFFFF00",
+    height:95,
+    duration:1,
+    color:"#fff",
+    scrollTrigger:{
+        trigger:".nav",
+        scroller:"body",
+        start:"top -20%",
+        end:"top -22%",
+        scrub:2
+    }
+
+    }
+)
 
 
 // End Kishan
