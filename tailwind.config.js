1431554965a87bb0145d89bcd30ea85d1b0c7d27
KEvin_axaz <kevin53846.914@gmail.com>
2024-07-01 15:31:07 +0000

    productadd -axaz

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
   theme: {
     extend: {
       fontFamily:{
-        f1:["f1"]
+        f1:["f1"],
+        f2:["f2"],
+        ubantu:["ubantu"]
+      },
+      boxShadow:{
+        sh1:'20px 20px 20px 20px black'
+      },
+      backgroundImage:{
+        'bg1':"url(../public/assets/icon/logoback.png)"
       }
     },
   },
