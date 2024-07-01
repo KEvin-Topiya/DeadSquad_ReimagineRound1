c945d174ed78237539eec4a76e3f0aba8859df17
Kish-v17 <kish.v07@gmail.com>
2024-07-01 18:50:24 +0530

    CustomCursor

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,8 @@
 //=============== Kishan code================
 var csr = document.querySelector("#cursor")
 document.addEventListener("mousemove",function(loc){
-    csr.style.left=loc.x-20+"px";
-    csr.style.top=loc.y-20+"px";
+    csr.style.left=loc.x-15+"px";
+    csr.style.top=loc.y-15+"px";
 })
 
 
